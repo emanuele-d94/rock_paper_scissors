6f24a3ee2d5e822fa67aa20007e7e4b897287213
emanuele-midori <emanuele@midorisrl.eu>
2026-02-25 18:26:32 +0100

    Rock Paper Scissors Game Definizione UI

--- script.js
+++ script.js
@@ -1,55 +1,115 @@
-
-let humanScore = 0
+let userScore = 0
 let computerScore = 0
+let round = 1
+const totalRounds = 5
+
+let userRockButton = document.querySelector('.user-rock')
+let userPaperButton = document.querySelector('.user-paper')
+let userScissorButton = document.querySelector('.user-scissor')
+
+let computerRockButton = document.querySelector('.computer-rock');
+let computerPaperButton = document.querySelector('.computer-paper');
+let computerScissorButton = document.querySelector('.computer-scissor');
+
+function getUserChoice() {
+
+    return new Promise((resolve, reject) => {
+        userRockButton.addEventListener('click', function () {
+            userRockButton.style.backgroundColor = 'green'
+            userRockButton.style.border = 'solid 2px green'
+
+            userPaperButton.disabled = true
+            userScissorButton.disabled = true
+
+            console.log('User choice: rock');
+            resolve('rock')
+
+        })
+
+        userPaperButton.addEventListener('click', function () {
+            userPaperButton.style.backgroundColor = 'green'
+            userPaperButton.style.border = 'solid 2px green'
+
+            userRockButton.disabled = true
+            userScissorButton.disabled = true
+
+            console.log('User choice: paper');
+            resolve('paper')
+
+        })
+
+        userScissorButton.addEventListener('click', function () {
+            userScissorButton.style.backgroundColor = 'green'
+            userScissorButton.style.border = 'solid 2px green'
+
+            userRockButton.disabled = true
+            userPaperButton.disabled = true
+
+            console.log('User choice: scissor');
+            resolve('scissor')
+
+        })
+    })
+
+}
 
 
-function getComputerChoice(){
+function getComputerChoice() {
+
     let computerChoice
     let n = Math.random() * 100
 
-    if(n >= 0 && n < 33.3){
-        computerChoice = "rock"
-    } else if(n >= 33.3 && n < 66.6){
-        computerChoice = "paper"
+    if (n >= 0 && n < 33.3) {
+        computerChoice = 'rock'
+
+        computerRockButton.style.border = 'solid 2px red'
+        computerRockButton.style.backgroundColor = 'red'
+
+        computerPaperButton.disabled = true
+        computerScissorButton.disabled = true
+        console.log('Computer choice: ' + computerChoice);
+        return computerChoice
+    } else if (n >= 33.3 && n < 66.6) {
+        computerChoice = 'paper'
+
+        computerPaperButton.style.border = 'solid 2px red'
+        computerPaperButton.style.backgroundColor = 'red'
+
+        computerRockButton.disabled = true
+        computerScissorButton.disabled = true
+        console.log('Computer choice: ' + computerChoice);
+        return computerChoice
     } else {
-        computerChoice = "scissors"
+        computerChoice = 'scissor'
+
+        computerScissorButton.style.border = 'solid 2px red'
+        computerScissorButton.style.backgroundColor = 'red'
+
+        computerRockButton.disabled = true
+        computerPaperButton.disabled = true
+        console.log('Computer choice: ' + computerChoice);
+        return computerChoice
     }
 
-    return computerChoice
+
 }
 
-function getHumanChoice(){
-    let admissibleChoices = ['rock', 'paper', 'scissors']
-    let humanChoice = null
 
-    while (humanChoice === null){
-        humanChoice = prompt("What do you want to play? Rock, paper or scissors?").toLowerCase();
-        if(!admissibleChoices.includes(humanChoice)){
-            console.log("Error, choose one between rock paper or scissors")
-            humanChoice = null
-        }
-    }
+function playRound(userChoice, computerChoice) {
 
-    return humanChoice
-}
-
-function playRound(humanChoice,computerChoice){
-    console.log("You play: " + humanChoice)
-    console.log("Computer plays: " + computerChoice)
-
-    if(humanChoice === "rock"){
+    if(userChoice === "rock"){
         if (computerChoice === "rock"){
             console.log("Tie!")
         } else if(computerChoice === "paper"){
             computerScore +=1
             console.log("Computer wins!")
         } else if(computerChoice === "scissors"){
-            humanScore +=1
+            userScore +=1
             console.log("You win!")
         }
-    } else if (humanChoice === "paper"){
+    } else if (userChoice === "paper"){
         if (computerChoice === "rock"){
-            humanScore +=1
+            userScore +=1
             console.log("You win!")
         } else if(computerChoice === "paper"){
             console.log("Tie!")
@@ -57,12 +117,12 @@ function playRound(humanChoice,computerChoice){
             computerScore +=1
             console.log("Computer wins!")
         }
-    } else if (humanChoice === "scissors"){
+    } else if (userChoice === "scissors"){
         if (computerChoice === "rock"){
             computerScore +=1
             console.log("Computer wins!")
         } else if(computerChoice === "paper"){
-            humanScore +=1
+            userScore +=1
             console.log("You win!")
         } else if(computerChoice === "scissors"){
             console.log("Tie!")
@@ -71,34 +131,48 @@ function playRound(humanChoice,computerChoice){
 
 }
 
-function playGame(){
+function aspetta(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
 
-    const total_rounds = 5
-    let played_rounds = 0
+function resetButtons() {
 
-    while(played_rounds < total_rounds){
+    userRockButton.removeAttribute('style');
+    userRockButton.disabled = false;
+    userPaperButton.removeAttribute('style');
+    userPaperButton.disabled = false;
+    userScissorButton.removeAttribute('style');
+    userScissorButton.disabled = false;
 
-        console.log("Round " + (played_rounds + 1) + " :")
+    computerRockButton.removeAttribute('style');
+    computerRockButton.disabled = false;
+    computerPaperButton.removeAttribute('style');
+    computerPaperButton.disabled = false;
+    computerScissorButton.removeAttribute('style');
+    computerScissorButton.disabled = false;
+}
 
-        let humanChoice = getHumanChoice()
+async function playGame() {
+
+    while (round <= totalRounds) {
+        console.log('Round: ' + round)
+
+        let userChoice = await getUserChoice()
+
         let computerChoice = getComputerChoice()
 
-        playRound(humanChoice,computerChoice)
+        playRound(userChoice, computerChoice)
 
-        played_rounds +=1
+        console.log('User score: '+ userScore);
+        console.log('Computer score: '+ computerScore);
+        console.log('End Round ' + round);
+        round +=1;
+        console.log('---')
 
-        console.log("Player score: " + humanScore)
-        console.log("Computer score: " + computerScore)
-
+        await aspetta(2000) // aspetta che l'utente prema qualcosa
+        resetButtons()
     }
 
-    if(humanScore > computerScore){
-        alert("You won the game!")
-    } else if(humanScore < computerScore){
-        alert("You lost the game!")
-    } else {
-        alert("No one won!")
-    }
 
 }
 
