f1ea54a28bbd763dec25234112c71fb29ac8017f
emanuele-midori <emanuele@midorisrl.eu>
2026-02-26 11:00:01 +0100

    Logiche di gioco completate

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ let computerScore = 0
 let round = 1
 const totalRounds = 5
 
+let roundNumber = document.querySelector('.round p')
+roundNumber.textContent = ''.concat(round,'/',totalRounds)
+
+let userScoreNumber = document.querySelector('.score-user p')
+userScoreNumber.textContent = userScore
+
+let computerScoreNumber = document.querySelector('.score-computer p')
+computerScoreNumber.textContent = computerScore
+
+let resultText = document.querySelector('.result h2')
+
 let userRockButton = document.querySelector('.user-rock')
 let userPaperButton = document.querySelector('.user-paper')
 let userScissorButton = document.querySelector('.user-scissor')
@@ -10,6 +21,11 @@ let userScissorButton = document.querySelector('.user-scissor')
 let computerRockButton = document.querySelector('.computer-rock');
 let computerPaperButton = document.querySelector('.computer-paper');
 let computerScissorButton = document.querySelector('.computer-scissor');
+
+let playAgainButton = document.querySelector('.play-again button')
+
+let playAgainArea = document.querySelector('.play-again')
+playAgainArea.classList.add('hidden')
 
 function getUserChoice() {
 
@@ -97,37 +113,61 @@ function getComputerChoice() {
 
 function playRound(userChoice, computerChoice) {
 
-    if(userChoice === "rock"){
-        if (computerChoice === "rock"){
-            console.log("Tie!")
-        } else if(computerChoice === "paper"){
+    let resultTextStyle = ''
+    let resultTextContent = ''
+
+    if(userChoice === 'rock'){
+        if (computerChoice === 'rock'){
+            console.log('Tie!')
+            resultTextStyle = 'color:blue'
+            resultTextContent = 'Tie!'
+        } else if(computerChoice === 'paper'){
             computerScore +=1
-            console.log("Computer wins!")
-        } else if(computerChoice === "scissors"){
+            console.log('Computer wins!')
+            resultTextStyle = 'color:red'
+            resultTextContent = 'Computer wins!'
+        } else if(computerChoice === 'scissor'){
             userScore +=1
-            console.log("You win!")
+            console.log('You win!')
+            resultTextStyle = 'color:green'
+            resultTextContent = 'You win!'
         }
-    } else if (userChoice === "paper"){
-        if (computerChoice === "rock"){
+    } else if (userChoice === 'paper'){
+        if (computerChoice === 'rock'){
             userScore +=1
-            console.log("You win!")
-        } else if(computerChoice === "paper"){
-            console.log("Tie!")
-        } else if(computerChoice === "scissors"){
+            console.log('You win!')
+            resultTextStyle = 'color:green'
+            resultTextContent = 'You win!'
+        } else if(computerChoice === 'paper'){
+            console.log('Tie!')
+            resultTextStyle = 'color:blue'
+            resultTextContent = 'Tie!'
+        } else if(computerChoice === 'scissor'){
             computerScore +=1
-            console.log("Computer wins!")
+            console.log('Computer wins!')
+            resultTextStyle = 'color:red'
+            resultTextContent = 'Computer wins!'
         }
-    } else if (userChoice === "scissors"){
-        if (computerChoice === "rock"){
+    } else if (userChoice === 'scissor'){
+        if (computerChoice === 'rock'){
             computerScore +=1
-            console.log("Computer wins!")
-        } else if(computerChoice === "paper"){
+            console.log('Computer wins!')
+            resultTextStyle = 'color:red'
+            resultTextContent = 'Computer wins!'
+        } else if(computerChoice === 'paper'){
             userScore +=1
-            console.log("You win!")
-        } else if(computerChoice === "scissors"){
-            console.log("Tie!")
+            console.log('You win!')
+            resultTextStyle = 'color:green'
+            resultTextContent = 'You win!'
+        } else if(computerChoice === 'scissor'){
+            console.log('Tie!')
+            resultTextStyle = 'color:blue'
+            resultTextContent = 'Tie!'
         }
     }
+
+    resultText.style = resultTextStyle
+    resultText.textContent = resultTextContent
 
 }
 
@@ -152,6 +192,64 @@ function resetButtons() {
     computerScissorButton.disabled = false;
 }
 
+function updateScore() {
+
+    if(round <= totalRounds){
+        roundNumber.textContent = ''.concat(round,'/',totalRounds)
+    }
+    userScoreNumber.textContent = userScore
+    computerScoreNumber.textContent = computerScore
+    resultText.textContent = ''
+}
+
+function resetGame(){
+
+    userScore = 0
+    computerScore = 0
+    round = 1
+    resetButtons()
+    roundNumber.textContent = ''.concat(round,'/',totalRounds)
+    userScoreNumber.textContent = userScore
+    computerScoreNumber.textContent = computerScore
+    resultText.textContent = ''
+    playAgainArea.classList.add('hidden');
+
+    playGame()
+}
+
+function finalResult() {
+
+    if(userScore > computerScore){
+        resultText.style = 'color:green'
+        resultText.textContent = 'You won the game!'
+    } else if (computerScore > userScore){
+        resultText.style = 'color:red'
+        resultText.textContent = 'Computer won the game!'
+    } else {
+        resultText.style = 'color:blue'
+        resultText.textContent = 'No one won!'
+    }
+
+    userRockButton.disabled = true;
+    userPaperButton.removeAttribute('style');
+    userPaperButton.disabled = true;
+    userScissorButton.removeAttribute('style');
+    userScissorButton.disabled = true;
+
+    computerRockButton.removeAttribute('style');
+    computerRockButton.disabled = true;
+    computerPaperButton.removeAttribute('style');
+    computerPaperButton.disabled = true;
+    computerScissorButton.removeAttribute('style');
+    computerScissorButton.disabled = true;
+
+    playAgainArea.classList.remove('hidden');
+    playAgainButton.addEventListener('click', function(){
+        resetGame();
+    })
+
+}
+
 async function playGame() {
 
     while (round <= totalRounds) {
@@ -166,12 +264,17 @@ async function playGame() {
         console.log('User score: '+ userScore);
         console.log('Computer score: '+ computerScore);
         console.log('End Round ' + round);
+
         round +=1;
+
         console.log('---')
 
-        await aspetta(2000) // aspetta che l'utente prema qualcosa
+        await aspetta(2000) // aspetta 2s
         resetButtons()
+        updateScore()
     }
+
+    finalResult()
 
 
 }
